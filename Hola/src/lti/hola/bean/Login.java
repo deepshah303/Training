db7f36a54c9e86f67d67739c8935ca13d184ac83
deepshah303 <41993655+deepshah303@users.noreply.github.com>
2018-08-06 22:56:20 +0530

    Update Login.java

--- Hola/src/lti/hola/bean/Login.java
+++ Hola/src/lti/hola/bean/Login.java
@@ -1,5 +1,23 @@
 package lti.hola.bean;
 
 public class Login {
+  private String emailBean;
+	private String passwordBean;
+
+	public String getEmailBean() {
+		return emailBean;
+	}
+
+	public void setEmailBean(String emailBean) {
+		this.emailBean = emailBean;
+	}
+
+	public String getPasswordBean() {
+		return passwordBean;
+	}
+
+	public void setPasswordBean(String passwordBean) {
+		this.passwordBean = passwordBean;
+	}
 
 }
