87488429367aada2c4dbc6b0dbc4dedcf7b16422
deepshah303 <41993655+deepshah303@users.noreply.github.com>
2018-08-06 22:57:28 +0530

    Update Register.java

--- Hola/src/lti/hola/bean/Register.java
+++ Hola/src/lti/hola/bean/Register.java
@@ -1,5 +1,78 @@
 package lti.hola.bean;
 
 public class Register {
+	private String nameBean;
+	private String emailBean;
+	private String passwordBean;
+	private int ageBean;
+	private String genderBean;
+	private String cityBean;
+	private String movieBean;
+	private String photoBean;
+
+	public String getNameBean() {
+		return nameBean;
+	}
+
+	public void setNameBean(String nameBean) {
+		this.nameBean = nameBean;
+	}
+
+	public String getEmailBean() {
+		return emailBean;
+	}
+
+	public void setEmailBean(String emailBean) {
+		this.emailBean = emailBean;
+	}
+
+	public String getPasswordBean() {
+		return passwordBean;
+	}
+
+	public void setPasswordBean(String passwordBean) {
+		this.passwordBean = passwordBean;
+	}
+
+	public int getAgeBean() {
+		return ageBean;
+	}
+
+	public void setAgeBean(int ageBean) {
+		this.ageBean = ageBean;
+	}
+
+	public String getGenderBean() {
+		return genderBean;
+	}
+
+	public void setGenderBean(String genderBean) {
+		this.genderBean = genderBean;
+	}
+
+	public String getCityBean() {
+		return cityBean;
+	}
+
+	public void setCityBean(String cityBean) {
+		this.cityBean = cityBean;
+	}
+
+	public String getMovieBean() {
+		return movieBean;
+	}
+
+	public void setMovieBean(String movieBean) {
+		this.movieBean = movieBean;
+	}
+
+	public String getPhotoBean() {
+		return photoBean;
+	}
+
+	public void setPhotoBean(String photoBean) {
+		this.photoBean = photoBean;
+	}
+
 
 }
